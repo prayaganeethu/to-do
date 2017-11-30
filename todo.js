6368b49d4d8b46f4db821d81c20d8057cf8c12e5
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-30 14:19:22 +0530

    Use jade as view engine. Render a webpage

--- todo.js
+++ todo.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
+const routes = require('./routes')
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Lets create an awesome to do app!'))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'jade')
+app.get('/', routes.index)
 
 app.listen(PORT, () => console.log('To-do app listening on port ', PORT))
