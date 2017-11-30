349923f966e5ac4dba2b1cddba519dfbff4bb9a8
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-30 16:55:27 +0530

    Use html instead of Jade. Use in-memory storage instead of mongo.

--- todo.js
+++ todo.js
@@ -6,8 +6,7 @@ const app = express()
 
 const PORT = process.env.PORT || 8080
 
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'jade')
+app.use(express.static(path.join(__dirname, '/views')))
 
 app.get('/', routes.index)
 
