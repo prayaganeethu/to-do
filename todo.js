cbcb49ddc4f7b3c1b077b8be451cbd18dc8c452a
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-30 13:46:24 +0530

    update dependencies, set port to 8080 if process.env.PORT is undefined

--- todo.js
+++ todo.js
@@ -1,6 +1,9 @@
 const express = require('express')
+
 const app = express()
+
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('Lets create an awesome to do app!'))
 
-app.listen(3000, () => console.log('To-do app listening on port 3000!'))
+app.listen(PORT, () => console.log('To-do app listening on port ', PORT))
