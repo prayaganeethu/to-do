2b9855a256235c7c672ee9255056ffdddf02fa16
Neethu Mohandas <prayaganeethu@gmail.com>
2017-12-02 12:18:32 +0530

    Add hover property to button. Remove listed tasks on clicking again

--- public/javascripts/tasksDone.js
+++ public/javascripts/tasksDone.js
@@ -3,11 +3,21 @@ const task = document.createElement('div')
 task.id = 'done-task'
 const parent = show.parentElement
 parent.appendChild(task)
-show.addEventListener('click', function () {
-  const parent = document.getElementById('done-task')
-  const li = document.createElement('li')
-  for (let task of done) {
-    li.appendChild(document.createTextNode(task))
-    parent.appendChild(li)
+show.addEventListener('click', function (e) {
+  if (e.target.clicked) {
+    e.target.clicked = false
+    e.target.parentElement.removeChild(task)
+  } else if (!e.target.clicked || e.target.clicked === undefined) {
+    // const parent = document.getElementById('done-task')
+    const li = document.createElement('li')
+    li.id = 'done'
+    li.class = 'done'
+    const label = document.createElement('label')
+    label.htmlFor = 'done'
+    for (let todo of done) {
+      li.appendChild(label.appendChild(document.createTextNode(todo)))
+      task.appendChild(li)
+    }
+    e.target.clicked = true
   }
 })
